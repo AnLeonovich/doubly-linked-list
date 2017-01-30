9979c8c92b4b898725cc8c2b2bd63af89d9551e4
Nova <novka666@mail.ru>
2017-01-30 12:35:18 +0300

    doubly-linked-list/feature: more code

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,64 +1,80 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    
+    function constructor() {
+    this._length = 0;
+    this.head = null;
+    this.tail = null;
 
-    append(data) {
+    }
 
-        
-        this.addAtEnd = function (val) {
-        if (this.tail === null) {  //If first node
-            this.tail = new Node(val);
-            this.head = this.tail;
-        } else {
-            var temp = new Node(val);
-            temp.prev = this.tail;
-            this.tail.next = temp;
-            this.tail = temp;
-        }
+    function append(data) {
+var node = this._createNewNode(data);
+
+    if (this._head === null) {
+
+      // we are empty, so this is the first node
+      // use the same logic as append
+      return this.append(data);
+    } else {
+
+      // place before head
+      this._head.prev = node;
+      node.next = this._head;
+      this._head = node;
+    }
+
+    // update count
+    this._length++;
+
+    return node;
+  };
+    }
+
+   function head() {
+    return this._head;
+    }
+
+   function tail() {
+    return this._tail;
+    }
+
+   function at(index) {
+      if (index >= 0 && index < this._length) {
+      var node = this._head;
+      while (index--) {
+      node = node.next;
+      }
+      return node;
+    }
+
     };
+
+    function insertAt(index, data) {}
+
+   function isEmpty() {
+        if (this._length=0) {
+      return true;
+    } else {
+      return false;
+  }
+
     }
 
-    head() {}
-
-    tail() {}
-
-    at(index) {
-
-    var currentNode = this.head,
-        length = this._length,
-        count = 1,
-        message = {failure: 'Failure: non-existent node in this list.'};
-
-    // 1-ый случай: неверная позиция 
-    if (length === 0 || position < 1 || position > length) {
-        throw new Error(message.failure);
+    function clear() {
+         while (!this.isEmpty()) {
+                this.remove();
+            }
     }
 
-    // 2-ой случай: верная позиция 
-    while (count < position) {
-        currentNode = currentNode.next;
-        count++;
+    function deleteAt(index) {
+
     }
 
-    return currentNode;
-    }
+   function  reverse() {}
 
-    insertAt(index, data) {}
-
-    isEmpty() {}
-
-    clear() {}
-
-    deleteAt(index) {}
-
-    reverse() {}
-
-    indexOf(data) {}
+    function indexOf(data) {}
 }
 
 module.exports = LinkedList;
-
-function LinkedList() {
-    this.head = null;
-    this.tail = null;
